489137ea4e778ae92ed3816238513a4bb5246f47
Pragati Sharma <69748311+Pragati-11S@users.noreply.github.com>
2022-02-12 20:58:27 +0530

    8th & 9th feb assignment

---
+++ Week2/Assignment_09Feb.sql
@@ -0,0 +1,41 @@
+--Oracle Synonyms--
+SELECT * FROM lion.sales;
+
+CREATE PUBLIC SYNONYM sales FOR lion.sales; 
+
+SELECT * FROM sales; 
+
+--Oracle Sequence--
+CREATE SEQUENCE item_seq;
+
+SELECT item_seq.NEXTVAL
+FROM dual;
+
+SELECT item_seq.CURRVAL
+FROM dual;
+
+SELECT item_seq.NEXTVAL
+FROM   dual
+CONNECT BY level <= 5;
+
+CREATE TABLE items(
+    item_id NUMBER
+);
+
+INSERT INTO items(item_id) VALUES(item_seq.NEXTVAL);
+INSERT INTO items(item_id) VALUES(item_seq.NEXTVAL);
+
+COMMIT;
+
+SELECT item_id FROM items;
+
+DECLARE
+    v_seq NUMBER;
+BEGIN
+    v_seq  := item_seq.NEXTVAL;
+    DBMS_OUTPUT.put_line('v_seq=' || v_seq);
+END;
+
+ALTER SEQUENCE item_seq MAXVALUE 100;
+
+DROP SEQUENCE item_seq;
